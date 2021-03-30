3401efdc2799c2310547d994f4753f1dfa4ff482
Quertte <a.injen90@gmail.com>
2021-03-30 18:52:05 +0300

    4.1. Аргументы [#507713] Корректировка

--- src/ru/job4j/ArgMethod.java
+++ src/ru/job4j/ArgMethod.java
@@ -1,11 +1,14 @@
 package ru.job4j;
 public class ArgMethod {
-    public static void hello(String name) {
-        System.out.println("Hello, " + name);
+
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + " " + age);
     }
 
     public static void main(String[] args) {
         String name = "Petr Arsentev";
-        ArgMethod.hello(name);
+        int age = 33;
+
+        ArgMethod.hello(name, age);
     }
 }
