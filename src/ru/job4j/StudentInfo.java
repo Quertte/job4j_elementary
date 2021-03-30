ea1adafb0e3ba3a1c4c736f1a233e32a485d7d3d
Quertte <a.injen90@gmail.com>
2021-03-30 16:21:15 +0300

    3.3. Разбор первой программы. [#507722]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Andrew Nedogibchenko");
+        System.out.println("07.09.1990");
+    }
+}
