57722b1fbfc991327be645df202525ddc52450fb
Quertte <a.injen90@gmail.com>
2021-03-30 15:59:54 +0300

    3.1. Загрузка файлов на github [#507728]

--- test/ru/job4j/MainTest.java
+++ test/ru/job4j/MainTest.java
@@ -1,6 +1,10 @@
 package ru.job4j;
 
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -10,5 +14,13 @@ public class MainTest {
         int excepted = 1;
         int result = 1;
         assertThat(result, is(excepted));
+    }
+
+    @Test
+    public void checkOutHelloWorld(){
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Main.main(null);
+        assertThat(out.toString(), is("Hello world" + System.lineSeparator()));
     }
 }
