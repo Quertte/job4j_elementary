28819543d0f3dca475d09e63be618f48ce33d33e
Quertte <a.injen90@gmail.com>
2021-04-08 07:45:10 +0300

    4.3.1. Оператор && [#507655]

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -10,9 +10,22 @@ public class Cinema {
         }
     }
 
+    public static void permission(boolean allowParent, boolean hasMoney) {
+        if (allowParent && hasMoney) {
+            System.out.println("I can go to the cinema");
+        } else {
+            System.out.println("I can't");
+        }
+    }
+
     public static void main(String[] args) {
         Cinema.access(21);
         Cinema.access(16);
+
+        permission(true, true);
+        permission(true, false);
+        permission(false, true);
+        permission(false, false);
 
     }
 }
